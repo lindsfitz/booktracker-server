326ad23baa3a16fd2f67695ee8b756251cdbcd40
lindsfitz <lindsaytfitz@gmail.com>
2022-08-28 12:04:22 -0700

    starting friend routes

---
+++ controllers/api/friendRoutes.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = express.Router();
+const { User, Request } = require('../../models')
+
+
+// GET friends -- get all friends based on userid (search both userid columns for matches)
+
+router.get('/:id', (req,res) => {
+    User.findByPk(req.params.id, {
+        include: [{
+            model:User,
+            as:'Sender'
+        },
+        {
+            model:User,
+            as:'Receiver'
+        }]
+    }).then(user => {
+        res.json(user)
+    }).catch(err => {
+        console.log(err)
+        res.json(err)
+    })
+})
+
+// POST friends -- create new Friend; 
+        // add sender to the currently logged in user
+        // user(currentUserId).addSender(senderIdfromRequest)
+        // then delete the request that matches the senderId & the receiverId
+
+router.post('/add/:sender',(req,res)=>{
+    User.findByPk(req.body.id).then(user => {
+        user.addReceiver(req.params.sender)
+        res.json({message:'Friend added successfully!'})
+    }).catch(err => {
+        console.log(err)
+        res.json(err)
+    })
+})
+
+// DELETE friend -- delete Friend where senderId & receiverId match
+
+// router.delete('/delete/:sender',(req,res)=> {
+
+// })
+
+// GET received requests -- get all requests where receiverId is userid
+
+// GET pending requests -- get all requests where senderId is userid
+
+// POST new request -- create a request where current user is SenderId
+
+// DELETE a request -- delete where current user is SenderId
+
+
+module.exports = router
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,11 +4,13 @@ const userRoutes = require("./userRoutes");
 const bookRoutes = require("./bookRoutes")
 const reviewRoutes = require("./reviewRoutes")
 const shelfRoutes = require("./shelfRoutes")
+const friendRoutes = require("./friendRoutes")
 
 
 router.use("/user",userRoutes)
 router.use("/book",bookRoutes)
 router.use("/review",reviewRoutes)
 router.use("/shelf",shelfRoutes)
+router.use("/friend",friendRoutes)
 
 module.exports = router;
