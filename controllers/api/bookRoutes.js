f397df2a58c7ced87cfc7afe335f6ed99e717c0a
lindsfitz <lindsaytfitz@gmail.com>
2022-09-08 17:11:33 -0700

    get all user books route working

--- controllers/api/bookRoutes.js
+++ controllers/api/bookRoutes.js
@@ -56,65 +56,128 @@ router.post('/addto/:shelfid', (req, res) => {
     })
 })
 
-router.get('/userbooks/:id', (req,res) => {
+router.get('/user/:id', async (req, res) => {
+    Shelf.findAll({
+        where: {
+            userId: req.params.id
+        }
+    }).then(async shelves => {
+
+        const userBooks = []
+
+        const testBooks = []
+
+        // const attempt = shelves.map(async shelf => {
+        //     const books = await shelf.getBooks({ joinTableAttributes: [], raw: true })
+        //     const single = books.map(book => {
+        //         userBooks.push(book);
+        //         return book;
+        //     })
+        //     return single;
+        // })
+
+        for (const shelf of shelves) {
+            const books = await shelf.getBooks({ joinTableAttributes: [], raw: true })
+            for (const book of books) {
+                userBooks.push(book)
+            }
+            
+        }
+
+        // shelves.forEach(async shelf => {
+        //     const books = await shelf.getBooks({ joinTableAttributes: [], raw: true })
+        //     books.forEach(book => userBooks.push(book))
+        // })
+
+        // for (let i = 0; i < shelves.length; i++) {
+        //     const element = shelves[i];
+
+        //     let books = await element.getBooks({ joinTableAttributes: [], raw: true })
+
+
+        //     for (let j = 0; j < books.length; j++) {
+        //         const test = books[j];
+        //         testBooks.push(test)
+        //     }
+
+        //     // userBooks.push(books)
+        // }
+
+        // console.log(attempt);
+        console.log(userBooks);
+        // console.log(userBooks)
+
+
+        return res.status(200).json(userBooks)
+
+    }).catch(err => {
+        console.log(err)
+        res.json(err)
+    })
+
+
+})
+
+
+// ALL USER BOOKS -- currently still returns shelf object rather than just books 
+router.get('/userbooks/:id', (req, res) => {
     Shelf.findAll({
         where: {
             userId: req.params.id
         },
-        // group:['UserId'],
-        // raw:true,
-        attributes:[],
+        attributes: [],
         include: [{
-            model:Book,
-            through: {attributes: []},
+            model: Book,
+            right: true,
+            through: { attributes: [] },
         }],
     })
-    .then(shelves => res.json(shelves))
-    .catch(err => {
-        console.log(err)
-        res.json(err)
-    })
+        .then(shelves => res.json(shelves))
+        .catch(err => {
+            console.log(err)
+            res.json(err)
+        })
 })
 
 // LIST OF ALL USER BOOKS MARKED AS 'READ'
-router.get('/read/:id', (req,res) => {
+router.get('/read/:id', (req, res) => {
     Book.findAll({
         where: {
-            '$Reviews.UserId$':req.params.id,
-            '$Reviews.read$':true
+            '$Reviews.UserId$': req.params.id,
+            '$Reviews.read$': true
         },
-        include:[{
-            model:Review,
-            attributes:[]
+        include: [{
+            model: Review,
+            attributes: []
         }]
     }).then(books => res.json(books))
-    .catch(err => {
-        console.log(err)
-        res.json(err)
-    })
+        .catch(err => {
+            console.log(err)
+            res.json(err)
+        })
 })
 
 
 // ALL-TIME STATS -- RETURNS BOOKCOUNT, TOTALPAGES, AVG RATING for ALL user reviewed & read books
 
-router.get('/readstats/:id', (req,res) => {
+router.get('/readstats/:id', (req, res) => {
     Book.findAll({
         where: {
-            '$Reviews.UserId$':req.params.id,
-            '$Reviews.read$':true
+            '$Reviews.UserId$': req.params.id,
+            '$Reviews.read$': true
         },
-        attributes:[[sequelize.fn('count',sequelize.col('Book.id')),'bookCount'],[sequelize.fn("sum",sequelize.col("pages")), 'totalPages']],
-        include:[{
-            model:Review,
-            attributes:[[sequelize.fn('avg',sequelize.col('rating')),'avgRating']]
+        attributes: [[sequelize.fn('count', sequelize.col('Book.id')), 'bookCount'], [sequelize.fn("sum", sequelize.col("pages")), 'totalPages']],
+        include: [{
+            model: Review,
+            attributes: [[sequelize.fn('avg', sequelize.col('rating')), 'avgRating']]
         }],
-        group:['Reviews.UserId'],
-        raw:true
+        group: ['Reviews.UserId'],
+        raw: true
     }).then(books => res.json(books))
-    .catch(err => {
-        console.log(err)
-        res.json(err)
-    })
+        .catch(err => {
+            console.log(err)
+            res.json(err)
+        })
 })
 
 /* get route for user stats -- query for total number of books/year 
@@ -135,18 +198,18 @@ Book.findAll({
 
 // TOTAL PAGE # FOR ALL BOOKS ON USER SHELF -- Read & Unread 
 
-router.get('/stats/:id', (req,res) => {
+router.get('/stats/:id', (req, res) => {
     Shelf.findAll({
         where: {
             userId: req.params.id
         },
-        group:['UserId'],
-        raw:true,
-        attributes:['UserId'],
+        group: ['UserId'],
+        raw: true,
+        attributes: ['UserId'],
         include: [{
-            model:Book,
-            attributes:[[sequelize.fn("sum",sequelize.col("Books.pages")), 'totalPages']],
-            through: {attributes: []},
+            model: Book,
+            attributes: [[sequelize.fn("sum", sequelize.col("Books.pages")), 'totalPages']],
+            through: { attributes: [] },
             // include:[{
             //     model:Review,
             //     where: {
@@ -157,11 +220,11 @@ router.get('/stats/:id', (req,res) => {
             // }]
         }],
     })
-    .then(shelves => res.json(shelves))
-    .catch(err => {
-        console.log(err)
-        res.json(err)
-    })
+        .then(shelves => res.json(shelves))
+        .catch(err => {
+            console.log(err)
+            res.json(err)
+        })
 })
 
 // router.get('/stats/:id', (req, res) => {
